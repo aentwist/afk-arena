ce1414dd169d178167eae53beeb018c37b8fc798
Anderson Entwistle <acentwistle01@gmail.com>
2023-05-22 11:22:42 -0400

    build: use Vite Vuetify plugin
    Enable treeshaking.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,12 +1,7 @@
 import "vuetify/styles";
 import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+const vuetify = createVuetify();
 
 export default vuetify;
