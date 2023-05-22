dd2a2583863324131a92cfbe7fa65f043527ad67
Anderson Entwistle <acentwistle01@gmail.com>
2023-05-22 11:28:55 -0400

    build: use JS SVG icons
    Only bundle icons that are used.

--- src/components/InformationIcon.vue
+++ src/components/InformationIcon.vue
@@ -1,13 +1,15 @@
 <template>
   <v-tooltip location="bottom">
     <template #activator="{ props }">
-      <v-icon v-bind="props">mdi-information</v-icon>
+      <v-icon :icon="mdiInformation" v-bind="props" />
     </template>
     <div>{{ text }}</div>
   </v-tooltip>
 </template>
 
 <script setup lang="ts">
+import { mdiInformation } from "@mdi/js";
+
 defineProps<{
   text: string;
 }>();
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,15 @@
 import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 
 export default vuetify;
