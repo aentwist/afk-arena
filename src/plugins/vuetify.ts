62dafd616cdc5149cbad77e2a447186a5cd2aa88
Anderson Entwistle <acentwistle01@gmail.com>
2023-08-01 00:13:03 -0400

    build: use Vuetify Labs components

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,10 @@
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import * as labsComponents from "vuetify/labs/components";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
 const vuetify = createVuetify({
+  components: labsComponents,
   icons: {
     defaultSet: "mdi",
     aliases,
