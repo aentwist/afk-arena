e6123fed0454f942ab0529d137b658cf1b81ae02
Anderson Entwistle <46688047+aentwist@users.noreply.github.com>
2023-10-20 11:21:23 -0400

    feat: rebuild calculator

--- src/views/AbexView.vue
+++ src/views/AbexView.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="mx-1 mx-sm-0 my-12 d-flex flex-column align-center">
-    <div class="mb-8 d-flex align-center">
+  <v-alert
+    v-model="showQuietTimeWarning"
+    type="warning"
+    :text="`Quiet Time starts ${quietTimeStartDateAgo}`"
+    closable
+  />
+
+  <div class="my-3 my-sm-12 d-flex flex-column align-center">
+    <div class="mx-1 mx-sm-0 mb-3 mb-sm-8 d-flex align-center">
       <h1 class="me-1 text-h4 text-md-h3 line-clamp-1">Attack Calculator</h1>
-      <v-btn
-        v-if="page > SETTINGS_PAGE"
-        :icon="mdiCog"
-        variant="plain"
-        @click="page = SETTINGS_PAGE"
-      />
+      <ToolbarActions v-if="page > SETTINGS_PAGE" :actions="toolbarActions" />
     </div>
 
-    <div class="page-container">
+    <div class="w-100">
       <div v-if="page === WELCOME_PAGE">
         <h3 class="mb-8 text-h5 text-center">
           Welcome to AE {{ season.title }}!
@@ -69,88 +71,147 @@
         </v-btn>
       </v-form>
 
-      <v-form v-if="page === CALC_PAGE">
-        <!-- TODO: Only allow positive integers -->
-        <v-text-field
-          v-model.number="stamina"
-          class="required"
-          label="Stamina"
-          type="number"
+      <div v-if="page === CALC_PAGE">
+        <v-table
+          class="mb-4"
+          :headers="headers"
+          :items="items"
+          :items-per-page="-1"
         >
-          <template #append-inner>
-            <InformationIcon
-              text="The stamina for the hero with the least amount in the team"
-            />
-          </template>
-        </v-text-field>
+          <thead>
+            <tr>
+              <th
+                v-for="header in headers"
+                :key="header.key"
+                class="text-no-wrap"
+              >
+                {{ header.title }}
+                <InformationIcon
+                  v-if="header.key === 'stamina'"
+                  text="The stamina for the hero with the least amount in the team"
+                  size="small"
+                />
+              </th>
+            </tr>
+          </thead>
 
-        <!-- TODO: Build theming into all VBtn -->
-        <v-btn
-          class="w-100 rounded-theme"
-          size="large"
-          variant="flat"
-          color="primary"
-          @click="nextPage"
-        >
-          Calculate
-        </v-btn>
-      </v-form>
+          <tbody>
+            <tr v-for="(item, i) in items" :key="i">
+              <td class="pa-0 col-group-end">
+                <v-text-field
+                  v-model.number="item.formation"
+                  class="required"
+                  type="text"
+                  hide-details
+                />
+              </td>
+              <td class="pa-0 col-group-end">
+                <!-- TODO: Only allow positive integers -->
+                <v-text-field
+                  v-model.number="item.stamina"
+                  class="required"
+                  type="number"
+                  hide-details
+                />
+              </td>
+              <td>
+                <div class="d-flex align-center">
+                  {{ item.attacks }}
+                  <v-btn
+                    size="small"
+                    variant="flat"
+                    :icon="mdiMinus"
+                    @click="item.stamina -= STAMINA_PER_ATTACK"
+                  />
+                </div>
+              </td>
+              <td class="col-group-end">
+                <div class="d-flex align-center">
+                  {{ item.rewinds }}
+                  <v-btn
+                    size="small"
+                    variant="flat"
+                    :icon="mdiMinus"
+                    @click="item.stamina -= STAMINA_PER_REWIND"
+                  />
+                </div>
+              </td>
+              <td class="pa-0 col-group-end">
+                <!-- TODO: Validate -->
+                <v-text-field
+                  v-model.number="item.damagePerAttack"
+                  type="number"
+                  hide-details
+                />
+              </td>
+              <td>{{ item.damage }}</td>
+            </tr>
+          </tbody>
 
-      <div v-if="page === RESULTS_PAGE">
-        <div class="mb-4">
-          <p class="mb-4">
-            There are
-            <strong>{{ Math.floor(hoursLeft) }} hours left</strong>
-            before quiet time starts.
-          </p>
-          <p class="mb-4">
-            Your team will accrue {{ Math.floor(staminaToGain) }} more stamina
-            from now and have a total of {{ totalStamina }} stamina, and will
-            therefore have <strong>{{ remainingFullAttacks }} attacks</strong>.
-          </p>
-          <p class="mb-4">
-            The attacks will use a total of
-            {{ remainingFullAttackStamina }} stamina,
-            <strong
-              >leaving
-              {{ totalStamina - remainingFullAttackStamina }} stamina </strong
-            >.
-          </p>
-          <p class="mb-8 text-h6">
-            Since each rewind costs 4 stamina, you can therefore
-            <strong>rewind {{ freeRewinds }} times</strong> without losing an
-            attack!
-          </p>
-        </div>
+          <tfoot>
+            <tr>
+              <th class="col-group-end"><strong>Total</strong></th>
+              <td class="col-group-end">{{ totalStamina }}</td>
+              <td>{{ totalAttacks }}</td>
+              <td class="col-group-end">{{ totalRewinds }}</td>
+              <td class="col-group-end highlight">
+                {{ totalDamagePerAttack }}
+              </td>
+              <td class="highlight">{{ totalDamage }}</td>
+            </tr>
+          </tfoot>
+        </v-table>
 
-        <v-btn
-          class="w-100 rounded-theme"
-          size="large"
-          variant="tonal"
-          color="primary"
-          @click="prevPage"
-        >
-          Back
-        </v-btn>
+        <v-dialog v-model="showNewCompForm">
+          <v-card>
+            <v-toolbar color="primary">
+              <v-btn :icon="mdiClose" @click="showNewCompForm = false" />
+              <v-toolbar-title class="text-center">New Comp</v-toolbar-title>
+              <v-toolbar-items>
+                <v-btn variant="text" @click="saveNewComp">Save</v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+
+            <section class="mt-3 mx-1">
+              <v-text-field
+                v-model="newCompFormation"
+                type="text"
+                label="Formation"
+                autofocus
+              />
+              <v-text-field
+                v-model.number="newCompStamina"
+                type="number"
+                label="Stamina"
+              />
+              <v-text-field
+                v-model.number="newCompDamagePerAttack"
+                type="number"
+                label="Damage per Attack (B)"
+              />
+            </section>
+          </v-card>
+        </v-dialog>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { useLocalStorage } from "@vueuse/core";
+import type { ToolbarAction } from "@/types";
+import { type ComputedRef, computed, reactive, type Ref, ref } from "vue";
+import { useLocalStorage, useTimeAgo } from "@vueuse/core";
 import { DateTime } from "luxon";
 import InformationIcon from "@/components/InformationIcon.vue";
-import { mdiCog } from "@mdi/js";
+import ToolbarActions from "@/components/ToolbarActions.vue";
+import { mdiClose, mdiCog, mdiMinus, mdiPlus } from "@mdi/js";
 
 const S_KEY_NS = "abex:";
 
-// Inputs
+// Settings
 const seasons = [
   {
     title: "S9",
-    // Convert titles to kebab-case for values
     value: "s9",
     quietTimeStartDate: DateTime.fromISO("2023-05-22T00:00Z"),
   },
@@ -161,7 +222,7 @@ const seasons = [
   },
   // {
   //   title: "S11 Beta",
-  //   value: "s11-beta",
+  //   value: "s11b",
   //   quietTimeStartDate: DateTime.fromISO("2023-07-23T00:00Z"),
   // },
 ];
@@ -190,10 +251,8 @@ isSod.value = settingsIsSod.value;
 const WELCOME_PAGE = 1;
 const SETTINGS_PAGE = 2;
 const CALC_PAGE = 3;
-const RESULTS_PAGE = 4;
 const page = ref(settingsComplete.value ? CALC_PAGE : WELCOME_PAGE);
 const nextPage = () => page.value++;
-const prevPage = () => page.value--;
 
 function setSettings() {
   // TODO: Validate settings page
@@ -207,41 +266,182 @@ function setSettings() {
   nextPage();
 }
 
-const stamina = ref(0);
+// Calculator
+// Givens
+const STAMINA_PER_HOUR = 4;
 
-// Results
 const hoursLeft = computed(() =>
   season.value.quietTimeStartDate.diff(DateTime.now()).as("hours")
 );
 const staminaToGain = computed(() => {
   let adjustmentFactor = 1 + 0.012 * spectators.value;
   if (isSod.value) adjustmentFactor += 0.1;
-  const staminaGain = 4 * adjustmentFactor;
+  const staminaGain = STAMINA_PER_HOUR * adjustmentFactor;
   return hoursLeft.value > 0 ? staminaGain * hoursLeft.value : 0;
 });
-const totalStamina = computed(() =>
-  Math.floor(stamina.value + staminaToGain.value)
+
+// Quiet time warning
+const showQuietTimeWarning = ref(Boolean(hoursLeft.value < 48));
+const quietTimeStartDateAgo = useTimeAgo(
+  season.value.quietTimeStartDate.toJSDate(),
+  {
+    showSecond: true,
+    rounding: "ceil",
+    updateInterval: 1_000,
+  }
 );
-const remainingFullAttacks = computed(() =>
-  Math.floor(totalStamina.value / 48)
-);
-const remainingFullAttackStamina = computed(
-  () => remainingFullAttacks.value * 48
-);
-const freeRewinds = computed(() => {
-  const residualStamina = totalStamina.value % 48;
-  return Math.floor(residualStamina / 4);
-});
+
+// Calculations
+const STAMINA_PER_ATTACK = 48;
+const STAMINA_PER_REWIND = 4;
+
+const getTotalStamina = (stamina: number) =>
+  Math.floor(stamina + staminaToGain.value);
+const getRemainingFullAttacks = (totalStamina: number) =>
+  Math.floor(totalStamina / STAMINA_PER_ATTACK);
+const getFreeRewinds = (totalStamina: number) => {
+  const residualStamina = totalStamina % STAMINA_PER_ATTACK;
+  return Math.floor(residualStamina / STAMINA_PER_REWIND);
+};
+
+// Computed factories
+function attacks(stamina: Ref<number>) {
+  return computed(() => {
+    const totalStamina = getTotalStamina(stamina.value);
+    return getRemainingFullAttacks(totalStamina);
+  });
+}
+function rewinds(stamina: Ref<number>) {
+  return computed(() => {
+    const totalStamina = getTotalStamina(stamina.value);
+    return getFreeRewinds(totalStamina);
+  });
+}
+const damage = (attacks: ComputedRef<number>, damagePerAttack: Ref<number>) =>
+  computed(() => attacks.value * damagePerAttack.value);
+
+// Inputs / Results / Table
+// TODO: db
+interface Comp {
+  formation: string; // TODO: make Formation
+  stamina: number;
+}
+interface CompItem {
+  formation: Ref<string>; //
+  stamina: Ref<number>;
+  attacks: ComputedRef<number>;
+  rewinds: ComputedRef<number>;
+  damagePerAttack: Ref<number>;
+  damage: ComputedRef<number>;
+}
+
+const headers = [
+  { title: "Formation", key: "formation" },
+  { title: "Stamina", key: "stamina" },
+  { title: "Attacks", key: "attacks" },
+  { title: "Rewinds", key: "rewinds" },
+  { title: "Damage per Attack (B)", key: "damagePerAttack" },
+  { title: "Remaining Damage (B)", key: "damage" },
+];
+
+const initRowStamina = ref(1_000);
+const initAttacks = attacks(initRowStamina);
+const initDamagePerAttack = ref(8);
+const items = reactive<CompItem[]>([
+  {
+    formation: ref("My Formation"),
+    stamina: initRowStamina,
+    attacks: initAttacks,
+    rewinds: rewinds(initRowStamina),
+    damagePerAttack: initDamagePerAttack,
+    damage: damage(initAttacks, initDamagePerAttack),
+  },
+]);
+
+const showNewCompForm = ref(false);
+const newCompFormation = ref("");
+const newCompStamina = ref(0);
+const newCompDamagePerAttack = ref(0);
+function saveNewComp() {
+  // TODO: no db for now - just add the thing
+  const stamina = ref(newCompStamina.value);
+  const atks = attacks(stamina);
+  const damagePerAttack = ref(newCompDamagePerAttack.value);
+  items.push({
+    formation: ref(newCompFormation.value),
+    stamina: stamina,
+    attacks: atks,
+    rewinds: rewinds(stamina),
+    damagePerAttack: damagePerAttack,
+    damage: damage(atks, damagePerAttack),
+  });
+
+  // Handle the form
+  showNewCompForm.value = false;
+  newCompFormation.value = "";
+  newCompStamina.value = 0;
+  newCompDamagePerAttack.value = 0;
+}
+
+function total(field: string) {
+  return computed(() => {
+    let sum = 0;
+    items.forEach((item) => void (sum += item[field]));
+    return sum;
+  });
+}
+const totalStamina = total("stamina");
+const totalAttacks = total("attacks");
+const totalRewinds = total("rewinds");
+const totalDamagePerAttack = total("damagePerAttack");
+const totalDamage = total("damage");
+
+const toolbarActions: ToolbarAction[] = [
+  {
+    title: "New Comp",
+    icon: mdiPlus,
+    handler: () => void (showNewCompForm.value = true),
+  },
+  {
+    title: "Settings",
+    icon: mdiCog,
+    handler: () => void (page.value = SETTINGS_PAGE),
+  },
+];
 </script>
 
 <style scoped>
-.page-container {
-  width: min(100%, var(--max-width));
+.col-group-end {
+  border-right: 1px solid rgb(var(--v-theme-on-surface-variant));
+}
+
+.highlight {
+  background-color: rgba(var(--v-theme-primary), var(--v-idle-opacity));
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+/* Remove spin buttons */
+/* Chrome */
+:deep(input::-webkit-outer-spin-button),
+:deep(input::-webkit-inner-spin-button) {
+  display: none;
+}
+/* Firefox */
+:deep(input[type="number"]) {
+  -moz-appearance: textfield;
 }
 
 strong {
   font-weight: bold;
   font-size: larger;
+}
+
+:deep(table) {
+  border-collapse: collapse;
+}
+
+tfoot tr {
+  border-top: medium solid rgba(var(--v-border-color), var(--v-border-opacity));
 }
 
 .v-checkbox :deep(.v-input__append) {
