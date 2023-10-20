c85e9a86f9ac8030c7f36d8b2f7225cb7879ec88
Anderson Entwistle <46688047+aentwist@users.noreply.github.com>
2023-10-20 11:19:46 -0400

    fix: allow mobile users to view tooltips

--- src/components/InformationIcon.vue
+++ src/components/InformationIcon.vue
@@ -1,16 +1,26 @@
 <template>
-  <v-tooltip location="bottom">
+  <v-tooltip v-model="show" location="bottom">
     <template #activator="{ props }">
-      <v-icon :icon="mdiInformation" v-bind="props" />
+      <v-icon
+        :icon="mdiInformation"
+        :size="size"
+        v-bind="props"
+        @click.stop="show = !show"
+      />
     </template>
     <div>{{ text }}</div>
   </v-tooltip>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { mdiInformation } from "@mdi/js";
 
 defineProps<{
   text: string;
+  // TODO: type
+  size?: string;
 }>();
+
+const show = ref(false);
 </script>
